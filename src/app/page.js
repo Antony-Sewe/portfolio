2dd91545cdc7bf37227f1f20fd96965badc29986
Antony-Sewe <antonywing6@gmail.com>
2025-03-04 16:52:32 +0300

    second commit

--- src/app/page.js
+++ src/app/page.js
@@ -1,19 +1,21 @@
 import Image from "next/image";
 import HeroSection from "@/components/HeroSection";
+import Navbar from "@/components/Navbar";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col bg-[#121212]">
-      <HeroSection/>
-      {/*
       <Navbar/>
       <div className="container mx-auto mt-24 px-12 py-4">
+        <HeroSection/>
         
+      </div>
+      
+      {/*
         <AchievementsSection/>
         <AboutSection/>
         <ProjectsSection/>
         <EmailSection/>
-      </div>
       <Footer/>
       */
       }
